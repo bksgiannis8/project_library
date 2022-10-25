5e1775bea5242606ce829191c3ade01801a07cc3
giannis <bkgiannis8@gmail.com>
2022-10-26 01:10:12 +0300

    Create the DOM elements to get the inputs

--- library.js
+++ library.js
@@ -8,24 +8,28 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     
     this.info = function() {
       return title + " " + "by" + " " + author + "," + " " + pages + " " + "pages," + " " + read;
     }
-  }
+}
   
 
 // function to add a book to the librarys
 function addBookToLibrary() {
-    let bookTitle = prompt()
-    let bookAuthor = prompt()
-    let bookPages = prompt()
-    let bookRead = prompt()
+    
+    let getTitle = document.getElementById("title") // DOM to take the inputs of title
+    let getAuthor = document.getElementById("author") // DOM to take the inputs of author
+    let getPages = document.getElementById("pages") // DOM to take the inputs of pages
+    let getRead = document.getElementById("read") // DOM to take the inputs of read
 
     // creating the new book
-    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    let newBook = new Book(getTitle, getAuthor, getPages, getRead);
     myLibrary.push(newBook);
 
 }
 
 addBookToLibrary()
+
+console.log(myLibrary)
