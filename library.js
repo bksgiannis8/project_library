7c94ef7a735bfcc86ba99fb2bb487a37c3945249
giannis <bkgiannis8@gmail.com>
2022-10-25 23:19:35 +0300

    Add a function to insert the books in an array

--- library.js
+++ library.js
@@ -10,19 +10,22 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     
     this.info = function() {
-      return title + " " + "by" + " " + author + "," + " " + pages + " " + "pages," + " " + read
+      return title + " " + "by" + " " + author + "," + " " + pages + " " + "pages," + " " + read;
     }
   }
   
 
-// function to add a book to the library
+// function to add a book to the librarys
 function addBookToLibrary() {
-    let title = input
-    let author = input
-    let pages = input
-    let read = input
+    let bookTitle = prompt()
+    let bookAuthor = prompt()
+    let bookPages = prompt()
+    let bookRead = prompt()
 
     // creating the new book
-    let newBook = new Book(title, author, pages, read)
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    myLibrary.push(newBook);
 
 }
+
+addBookToLibrary()
