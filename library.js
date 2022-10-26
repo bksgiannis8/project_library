7418aabf6c1f7f88497ce58d861004b8c2552e0a
giannis <bkgiannis8@gmail.com>
2022-10-26 03:14:52 +0300

    Fix the DOM elements to work

--- library.js
+++ library.js
@@ -1,6 +1,5 @@
 // array to store all of the book objects
-let myLibrary = [];
-
+let myLibrary = []; 
 
 // Book function constructor
 function Book(title, author, pages, read) {
@@ -9,27 +8,29 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    
-    this.info = function() {
-      return title + " " + "by" + " " + author + "," + " " + pages + " " + "pages," + " " + read;
-    }
 }
   
-
-// function to add a book to the librarys
+// function to add a book to the library
 function addBookToLibrary() {
     
-    let getTitle = document.getElementById("title") // DOM to take the inputs of title
-    let getAuthor = document.getElementById("author") // DOM to take the inputs of author
-    let getPages = document.getElementById("pages") // DOM to take the inputs of pages
-    let getRead = document.getElementById("read") // DOM to take the inputs of read
+    // DOM elements
+    let getTitle = document.getElementById('title').value; // DOM to take the inputs of title
+    let getAuthor = document.getElementById('author').value; // DOM to take the inputs of author
+    let getPages = document.getElementById('pages').value; // DOM to take the inputs of pages
+    let getRead = document.querySelector('input[name="read"]:checked').value; // DOM to take the inputs of read
+    console.log(getTitle, getAuthor, getPages, getRead)
 
     // creating the new book
     let newBook = new Book(getTitle, getAuthor, getPages, getRead);
     myLibrary.push(newBook);
+    console.log(getTitle, getAuthor, getPages, getRead)
+    console.log(myLibrary)
 
 }
 
-addBookToLibrary()
+// Give the ADD BOOK button an onclick event to add a book to the library
+let button = document.getElementById("add-btn");
+button.addEventListener("click", addBookToLibrary);
 
-console.log(myLibrary)
+
+
