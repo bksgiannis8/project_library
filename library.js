be63b995583e235be7c5039d32d403a88958f62f
giannis <bkgiannis8@gmail.com>
2022-10-26 04:48:16 +0300

    Creating a function to add a new row to the html table and display a new book there

--- library.js
+++ library.js
@@ -1,9 +1,9 @@
 // array to store all of the book objects
 let myLibrary = []; 
 
+
 // Book function constructor
 function Book(title, author, pages, read) {
-    // the constructor...
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -18,19 +18,46 @@ function addBookToLibrary() {
     let getAuthor = document.getElementById('author').value; // DOM to take the inputs of author
     let getPages = document.getElementById('pages').value; // DOM to take the inputs of pages
     let getRead = document.querySelector('input[name="read"]:checked').value; // DOM to take the inputs of read
-    console.log(getTitle, getAuthor, getPages, getRead)
 
     // creating the new book
     let newBook = new Book(getTitle, getAuthor, getPages, getRead);
     myLibrary.push(newBook);
-    console.log(getTitle, getAuthor, getPages, getRead)
-    console.log(myLibrary)
-
+    console.log(myLibrary);
 }
+
+
+// Displaying the newly created book in a new row in the table
+function AddRow() {
+    const tBody = document.querySelector("#table");
+    tBody.innerHTML = "";
+    myLibrary.forEach((book) => {
+      const htmlBook = `
+        <tr>
+          <td>${book.name}</td>
+          <td>${book.author}</td>
+          <td>${book.pages}</td>
+          <td>${book.read}</td>
+        </tr>
+        `;
+      tBody.insertAdjacentHTML("afterbegin", htmlBook);
+    });
+}
+
+
 
 // Give the ADD BOOK button an onclick event to add a book to the library
 let button = document.getElementById("add-btn");
 button.addEventListener("click", addBookToLibrary);
+button.addEventListener("click", AddRow);
+
+
+
+
+ 
+
+  
+
+
 
 
 
